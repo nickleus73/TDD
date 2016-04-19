5e4758173ae6fe3c42b84a5f8a7c7ba9459d8b08
Nicolas MORICET <nmo@yujia.fr>
2016-04-19 12:05:18 +0200

    Added new test

---
+++ tests/TennisTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by nickleus on 19/04/16.
+ */
+public class TennisTest {
+    @Test
+    public void testinitialisationNouveauJeu()
+    {
+        new Jeu();
+    }
+}
